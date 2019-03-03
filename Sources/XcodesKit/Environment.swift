a18344dd300e51d809e9d1c91bd572d1007a1efb
Brandon Evans <brandon@brandonevans.ca>
2019-03-03 09:39:03 -0700

    Remove quotes from Process path arguments
    Not necessary when passing as args to Process like it is when passing in a shell

--- Sources/XcodesKit/Environment.swift
+++ Sources/XcodesKit/Environment.swift
@@ -17,15 +17,15 @@ struct Environment {
 var Current = Environment()
 
 struct Shell {
-    var spctlAssess: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.sbin.spctl, "--assess", "--verbose", "--type", "execute", "\"\($0.path)\"") }
-    var codesignVerify: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.codesign, "-vv", "-d", "\"\($0.path)\"") }
-    var unxip: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.xip, workingDirectory: $0.deletingLastPathComponent(), "--expand", "\"\($0.path)\"") }
+    var unxip: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.xip, workingDirectory: $0.deletingLastPathComponent(), "--expand", "\($0.path)") }
+    var spctlAssess: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.sbin.spctl, "--assess", "--verbose", "--type", "execute", "\($0.path)") }
+    var codesignVerify: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.codesign, "-vv", "-d", "\($0.path)") }
     var devToolsSecurityEnable: () -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.sudo, "/usr/sbin/DevToolsSecurity", "-enable") }
     var addStaffToDevelopersGroup: () -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.sudo, "/usr/sbin/dseditgroup", "-o", "edit", "-t", "group", "-a", "staff", "_developer") }
     var acceptXcodeLicense: (InstalledXcode) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.sudo, $0.path.join("/Contents/Developer/usr/bin/xcodebuild").string, "-license", "accept") }
     var runFirstLaunch: (InstalledXcode) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.sudo, $0.path.join("/Contents/Developer/usr/bin/xcodebuild").string, "-runFirstLaunch") }
     var buildVersion: () -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.sw_vers, "-buildVersion") }
-    var xcodeBuildVersion: (InstalledXcode) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.libexec.PlistBuddy, "-c", "\"Print :ProductBuildVersion\"", "\"\($0.path.string)/Contents/version.plist\"") }
+    var xcodeBuildVersion: (InstalledXcode) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.libexec.PlistBuddy, "-c", "Print :ProductBuildVersion", "\($0.path.string)/Contents/version.plist") }
     var getUserCacheDir: () -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.getconf, "DARWIN_USER_CACHE_DIR") }
     var touchInstallCheck: (String, String, String) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin/"touch", "\($0)com.apple.dt.Xcode.InstallCheckCache_\($1)_\($2)") }
 }
