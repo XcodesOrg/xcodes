2a78301be1f581b03fe30f88f8eaad9a9e24123c
Brandon Evans <brandon@brandonevans.ca>
2019-08-19 22:22:18 -0600

    Make Environment public

--- Sources/XcodesKit/Environment.swift
+++ Sources/XcodesKit/Environment.swift
@@ -9,64 +9,64 @@ import Path
  - SeeAlso: https://www.pointfree.co/episodes/ep18-dependency-injection-made-comfortable
  - SeeAlso: https://vimeo.com/291588126
  */
-struct Environment {
-    var shell = Shell()
-    var files = Files()
+public struct Environment {
+    public var shell = Shell()
+    public var files = Files()
 }
 
-var Current = Environment()
+public var Current = Environment()
 
-struct Shell {
-    var unxip: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.xip, workingDirectory: $0.deletingLastPathComponent(), "--expand", "\($0.path)") }
-    var spctlAssess: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.sbin.spctl, "--assess", "--verbose", "--type", "execute", "\($0.path)") }
-    var codesignVerify: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.codesign, "-vv", "-d", "\($0.path)") }
-    var validateSudoAuthentication: () -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.sudo, "-nv") }
-    var devToolsSecurityEnable: (String?) -> Promise<ProcessOutput> = { Process.sudo(password: $0, Path.root.usr.sbin.DevToolsSecurity, "-enable") }
-    var addStaffToDevelopersGroup: (String?) -> Promise<ProcessOutput> = { Process.sudo(password: $0, Path.root.usr.sbin.dseditgroup, "-o", "edit", "-t", "group", "-a", "staff", "_developer") }
-    var acceptXcodeLicense: (InstalledXcode, String?) -> Promise<ProcessOutput> = { Process.sudo(password: $1, $0.path.join("/Contents/Developer/usr/bin/xcodebuild"), "-license", "accept") }
-    var runFirstLaunch: (InstalledXcode, String?) -> Promise<ProcessOutput> = { Process.sudo(password: $1, $0.path.join("/Contents/Developer/usr/bin/xcodebuild"),"-runFirstLaunch") }
-    var buildVersion: () -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.sw_vers, "-buildVersion") }
-    var xcodeBuildVersion: (InstalledXcode) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.libexec.PlistBuddy, "-c", "Print :ProductBuildVersion", "\($0.path.string)/Contents/version.plist") }
-    var getUserCacheDir: () -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.getconf, "DARWIN_USER_CACHE_DIR") }
-    var touchInstallCheck: (String, String, String) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin/"touch", "\($0)com.apple.dt.Xcode.InstallCheckCache_\($1)_\($2)") }
+public struct Shell {
+    public var unxip: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.xip, workingDirectory: $0.deletingLastPathComponent(), "--expand", "\($0.path)") }
+    public var spctlAssess: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.sbin.spctl, "--assess", "--verbose", "--type", "execute", "\($0.path)") }
+    public var codesignVerify: (URL) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.codesign, "-vv", "-d", "\($0.path)") }
+    public var validateSudoAuthentication: () -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.sudo, "-nv") }
+    public var devToolsSecurityEnable: (String?) -> Promise<ProcessOutput> = { Process.sudo(password: $0, Path.root.usr.sbin.DevToolsSecurity, "-enable") }
+    public var addStaffToDevelopersGroup: (String?) -> Promise<ProcessOutput> = { Process.sudo(password: $0, Path.root.usr.sbin.dseditgroup, "-o", "edit", "-t", "group", "-a", "staff", "_developer") }
+    public var acceptXcodeLicense: (InstalledXcode, String?) -> Promise<ProcessOutput> = { Process.sudo(password: $1, $0.path.join("/Contents/Developer/usr/bin/xcodebuild"), "-license", "accept") }
+    public var runFirstLaunch: (InstalledXcode, String?) -> Promise<ProcessOutput> = { Process.sudo(password: $1, $0.path.join("/Contents/Developer/usr/bin/xcodebuild"),"-runFirstLaunch") }
+    public var buildVersion: () -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.sw_vers, "-buildVersion") }
+    public var xcodeBuildVersion: (InstalledXcode) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.libexec.PlistBuddy, "-c", "Print :ProductBuildVersion", "\($0.path.string)/Contents/version.plist") }
+    public var getUserCacheDir: () -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin.getconf, "DARWIN_USER_CACHE_DIR") }
+    public var touchInstallCheck: (String, String, String) -> Promise<ProcessOutput> = { Process.run(Path.root.usr.bin/"touch", "\($0)com.apple.dt.Xcode.InstallCheckCache_\($1)_\($2)") }
 }
 
-struct Files {
-    var fileExistsAtPath: (String) -> Bool = { FileManager.default.fileExists(atPath: $0) }
+public struct Files {
+    public var fileExistsAtPath: (String) -> Bool = { FileManager.default.fileExists(atPath: $0) }
 
-    func fileExists(atPath path: String) -> Bool {
+    public func fileExists(atPath path: String) -> Bool {
         return fileExistsAtPath(path)
     }
 
-    var moveItem: (URL, URL) throws -> Void = { try FileManager.default.moveItem(at: $0, to: $1) }
+    public var moveItem: (URL, URL) throws -> Void = { try FileManager.default.moveItem(at: $0, to: $1) }
 
-    func moveItem(at srcURL: URL, to dstURL: URL) throws {
+    public func moveItem(at srcURL: URL, to dstURL: URL) throws {
         try moveItem(srcURL, dstURL)
     }
 
-    var contentsAtPath: (String) -> Data? = { FileManager.default.contents(atPath: $0) }
+    public var contentsAtPath: (String) -> Data? = { FileManager.default.contents(atPath: $0) }
 
-    func contents(atPath path: String) -> Data? {
+    public func contents(atPath path: String) -> Data? {
         return contentsAtPath(path)
     }
 
-    var removeItem: (URL) throws -> Void = { try FileManager.default.removeItem(at: $0) }
+    public var removeItem: (URL) throws -> Void = { try FileManager.default.removeItem(at: $0) }
 
-    func removeItem(at URL: URL) throws {
+    public func removeItem(at URL: URL) throws {
         try removeItem(URL)
     }
 
-    var trashItem: (URL) throws -> URL = { try FileManager.default.trashItem(at: $0) }
+    public var trashItem: (URL) throws -> URL = { try FileManager.default.trashItem(at: $0) }
 
     @discardableResult
-    func trashItem(at URL: URL) throws -> URL {
+    public func trashItem(at URL: URL) throws -> URL {
         return try trashItem(URL)
     }
     
-    var createFile: (String, Data?, [FileAttributeKey: Any]?) -> Bool = { FileManager.default.createFile(atPath: $0, contents: $1, attributes: $2) }
+    public var createFile: (String, Data?, [FileAttributeKey: Any]?) -> Bool = { FileManager.default.createFile(atPath: $0, contents: $1, attributes: $2) }
     
     @discardableResult
-    func createFile(atPath path: String, contents data: Data?, attributes attr: [FileAttributeKey : Any]? = nil) -> Bool {
+    public func createFile(atPath path: String, contents data: Data?, attributes attr: [FileAttributeKey : Any]? = nil) -> Bool {
         return createFile(path, data, attr)
     }
 }
