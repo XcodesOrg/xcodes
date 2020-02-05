87d7338a3bdca887630c1efc0d62c457218489bf
Brandon Evans <brandon@brandonevans.ca>
2020-02-04 19:40:39 -0700

    Ignore malformed Xcode app bundles

--- Sources/XcodesKit/Environment.swift
+++ Sources/XcodesKit/Environment.swift
@@ -101,14 +101,10 @@ public struct Files {
     public var installedXcodes = XcodesKit.installedXcodes
 }
 private func installedXcodes() -> [InstalledXcode] {
-    let results = try! Path.root.join("Applications").ls().filter { entry in
-        guard entry.kind == .directory && entry.path.extension == "app" && !entry.path.isSymlink else { return false }
-        let infoPlistPath = entry.path.join("Contents").join("Info.plist")
-        let infoPlist = try! PropertyListDecoder().decode(InfoPlist.self, from: try! Data(contentsOf: infoPlistPath.url))
-        return infoPlist.bundleID == "com.apple.dt.Xcode"
-    }
-    let installedXcodes = results.map { $0.path }.compactMap(InstalledXcode.init)
-    return installedXcodes
+    ((try? Path.root.join("Applications").ls()) ?? [])
+        .filter { $0.isAppBundle && $0.infoPlist?.bundleID == "com.apple.dt.Xcode" }
+        .map { $0.path }
+        .compactMap(InstalledXcode.init)
 }
 
 public struct Network {
