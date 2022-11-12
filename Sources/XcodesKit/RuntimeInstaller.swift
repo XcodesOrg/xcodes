1c952ecc8227fd53c5433a27f9e340f43a53e672
Steven Magdy <stevenmagdy92@gmail.com>
2022-11-12 20:25:50 +0100

    Document runtime installing steps

--- Sources/XcodesKit/RuntimeInstaller.swift
+++ Sources/XcodesKit/RuntimeInstaller.swift
@@ -69,26 +69,35 @@ public class RuntimeInstaller {
 
     private func installFromPackage(dmgUrl: URL, runtime: DownloadableRuntime) async throws {
         Current.logging.log("Mounting DMG")
+        // 1-Mount DMG and get the mounted path
         let mountedUrl = try await mountDMG(dmgUrl: dmgUrl)
+        // 2-Get the first path under the mounted path, should be a .pkg
         let pkgPath = try! Path(url: mountedUrl)!.ls().first!.path
+        // 3-Create a caches directory (if it doesn't exist), and
+        // 4-Set its ownership to the current user (important because under sudo it would be owned by root)
         try Path.xcodesCaches.mkdir().setCurrentUserAsOwner()
         let expandedPkgPath = Path.xcodesCaches/runtime.identifier
         try? Current.files.removeItem(at: expandedPkgPath.url)
+        // 5-Expand (not install) the pkg to temporary path
         try await Current.shell.expandPkg(pkgPath.url, expandedPkgPath.url).asVoid().async()
         try await unmountDMG(mountedURL: mountedUrl)
         let packageInfoPath = expandedPkgPath/"PackageInfo"
+        // 6-Get the `PackageInfo` file contents from the expanded pkg
         let packageInfoContentsData = Current.files.contents(atPath: packageInfoPath.string)!
         var packageInfoContents = String(data: packageInfoContentsData, encoding: .utf8)!
         let runtimeFileName = "\(runtime.platform.shortName) \(runtime.simulatorVersion.version).simruntime"
         let runtimeDestination = Path("/Library/Developer/CoreSimulator/Profiles/Runtimes/\(runtimeFileName)")!
         packageInfoContents = packageInfoContents.replacingOccurrences(of: "<pkg-info", with: "<pkg-info install-location=\"\(runtimeDestination)\"")
+        // 7-Modify the `PackageInfo` file with a new `install-location`
         try Current.files.write(packageInfoContents.data(using: .utf8)!, to: packageInfoPath.url)
         let newPkgPath = Path.xcodesCaches/(runtime.identifier + ".pkg")
         try? Current.files.removeItem(at: newPkgPath.url)
+        // 8-Re-create the expanded pkg with the new modified `PackageInfo` file
         try await Current.shell.createPkg(expandedPkgPath.url, newPkgPath.url).asVoid().async()
         try Current.files.removeItem(at: expandedPkgPath.url)
         Current.logging.log("Installing Runtime")
         // TODO: Report progress
+        // 9-Install the newly created pkg (must be root to run)
         try await Current.shell.installPkg(newPkgPath.url, "/").asVoid().async()
         try Current.files.removeItem(at: newPkgPath.url)
     }
